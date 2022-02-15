bcc20a540b0bfabcfb70714a3fc8c20c49b21448
s.pyanov@gmail.com <NUjFcwmP!666999>
2022-02-15 16:21:02 +1000

    v 0.3

--- logger.go
+++ logger.go
@@ -6,8 +6,10 @@ func Add(level string, message string, err error) {
 	var error_message string
 	if err != nil {
 		error_message = err.Error()
+		fmt.Println(level+" | "+message+" : ", error_message)
 	} else {
 		error_message = ""
+		fmt.Println(level + " | " + message)
 	}
-	fmt.Println(level+" | "+message+" : ", error_message)
+
 }
