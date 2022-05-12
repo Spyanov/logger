3d97cbb700e069fc56f33c75e62fc60d995170e6
Stanislav Pyanov <s.pyanov@gmail.com>
2022-05-12 16:07:18 +1000

    Добавлен logdestenation

--- logger.go
+++ logger.go
@@ -1,6 +1,11 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+var LOG_DESTENATION string = ""
 
 func Add(level string, message string, err error) {
 	var error_message string
@@ -11,5 +16,18 @@ func Add(level string, message string, err error) {
 		error_message = ""
 		fmt.Println(level + " | " + message)
 	}
+	f, err := os.Open(LOG_DESTENATION)
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
 
+		}
+	}(f)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла лога")
+	}
+	_, err = f.WriteString(level + " | " + message + " : " + error_message)
+	if err != nil {
+		fmt.Println("Ошибка записи лога")
+	}
 }
